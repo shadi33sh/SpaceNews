a45fe1cea498a6b47496937a8a4344c79819d8b9
shadi33sh <122544994+shadi33sh@users.noreply.github.com>
2025-01-08 02:08:57 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,24 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+module.exports =  {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontSize: {
+        'h1': '2.25rem', // 36px
+        'h2': '1.875rem', // 30px
+        'h3': '1.5rem', // 24px
+        'p': '1rem', // 16px
+      }
+    },
   },
   plugins: [],
-};
+} satisfies Config;
